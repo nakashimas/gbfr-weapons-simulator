2375ec86d4c8dcf6bb897a255fb58dbf9a7c0681
nakasimas <nascor.neco@gmail.com>
2024-02-28 12:23:58 +0900

    rename status files

---
+++ docs/character.js
@@ -0,0 +1,97 @@
+const CHARACTER_STATUS = {
+  "narmaya": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "siegfried": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "katalina": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "io": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "rosetta": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "lancelot": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "eugen": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "vaseraga": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "ferry": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "rackam": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "zeta": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "charlotta": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "percival": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "vane": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "gran": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "ghandagoza": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "yodarha": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "id": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "cagliostro": {
+    "levelsStatus": [
+      {"health": 0, "attackPower": 0, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+}
---
+++ docs/weapons.js
@@ -0,0 +1,211 @@
+const WEAPONS_STATUS = {
+  "amenoHabakiri": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "narmaya",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "balmung": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "siegfried",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "blutgang": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "katalina",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "caduceus": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "io",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "daggerOfBahamutCoda": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "rosetta",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "damascusKnife": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "lancelot",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "draconicFire": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "eugen",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "ereshkigal": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "vaseraga",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "etherealLasher": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "ferry",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "freikugel": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "rackam",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "gaeBulg": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "zeta",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "galatine": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "charlotta",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "gottfried": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "percival",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "mjolnir": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "vane",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "sevenStarSword": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "gran",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "skyPiercer": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "ghandagoza",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "swordfish": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "yodarha",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "swordOfBahamut": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "id",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+  "zosimos": {
+    "maxLevel": 160,
+    "minLevel": 160,
+    "character": "cagliostro",
+    "levelsSkill": [
+      {"catastrophe": 25, "regen": 15, "damageCap": 5, "sigilBooster": 1},
+    ],
+    "levelsStatus": [
+      {"health": 99, "attackPower": 6085, "criticalHitRate": 0, "stunPower": 0},
+    ]
+  },
+}
